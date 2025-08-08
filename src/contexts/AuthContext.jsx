ce8e238263357425fb5ad3df6ea8813c4149a097
Mostafa Magdy <mostafa.magdy621@gmail.com>
2025-08-08 18:15:56 +0300

    fix auth email

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -33,10 +33,16 @@ export const AuthProvider = ({ children }) => {
     return () => subscription.unsubscribe()
   }, [])
 
+  const redirectUrl = `${window.location.origin}/`
+
   const signUp = async (email, password) => {
     const { data, error } = await supabase.auth.signUp({
       email,
       password,
+      options: {
+        emailRedirectTo: redirectUrl,
+
+      }
     })
     return { data, error }
   }
